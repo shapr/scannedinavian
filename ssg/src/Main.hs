03db3b927272ea70ac1efb1703d4161dd5a2d7af
Shae Erisson <shae@scannedinavian.com>
2024-11-11 17:41:32 -0500

    add bus/truck number/factor blog post

--- ssg/src/Main.hs
+++ ssg/src/Main.hs
@@ -110,20 +110,6 @@ main = hakyllWith config $ do
                 >>= saveSnapshot "content"
                 >>= loadAndApplyTemplate "templates/default.html" ctx
 
-    -- create ["archive.html"] $ do
-    --     route idRoute
-    --     compile $ do
-    --         posts <- recentFirst =<< loadAll "posts/*"
-    --         let archiveCtx =
-    --                 listField "posts" postCtx (return posts)
-    --                     `mappend` constField "title" "Archives"
-    --                     `mappend` defaultContext
-
-    --         makeItem ""
-    --             >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-    --             >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-    --             >>= relativizeUrls
-
     match "index.html" $ do
         route idRoute
         compile $ do
