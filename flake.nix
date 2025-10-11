72ddd533edb5ab28450dce21b2a459f1475cfdbf
Shae Erisson <shae@scannedinavian.com>
2025-10-10 20:19:49 -0400

    change to 25.05

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     ];
   };
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
